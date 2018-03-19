1586bcec009fb7542976a2db3bde831e3552f0fa
Emerson Costa <costtasemerson@gmail.com>
2018-03-19 10:59:57 -0300

    Adding method to save the deck on filesystem

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // New type dec
 type deck []string
@@ -28,4 +32,15 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	//Casting slice of s rings to slice of bytes
+	data := []byte(d.toString())
+	//Writing/Saving content on disk
+	return ioutil.WriteFile(filename, data, 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -2,13 +2,4 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, remainingOfDeck := deal(cards, 5)
-
-	hand.print()
-	remainingOfDeck.print()
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
