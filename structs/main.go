aa6bf25518da6588955adf98c1023d588f31ea3b
Emerson Costa <costtasemerson@gmail.com>
2018-03-21 19:12:29 -0300

    Using embedded type

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "jim@server.com",
+			zipCode: 12345,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
