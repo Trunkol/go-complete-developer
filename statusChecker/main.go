2b4b2b698beaae0d16b0736e16734b30afc96d41
Emerson Costa <costtasemerson@gmail.com>
2018-03-26 10:59:22 -0300

    add v1 of statusChecker

--- statusChecker/main.go
+++ statusChecker/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	links := []string{
+		"http://facebook.com",
+		"http://google.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(link + " is up")
 }
